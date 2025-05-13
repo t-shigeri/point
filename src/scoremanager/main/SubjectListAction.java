e6b9b732af7aa446eafa19c15ce7ba8c3b1aac6c
h-hamakawa <fko2447005@stu.o-hara.ac.jp>
2025-05-13 12:12:26 +0900

    びゅっ

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -1,66 +1,37 @@
 package scoremanager.main;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Subject;
 import dao.SubjectDao;
-import tool.Action;
 
-public class SubjectListAction extends Action {
+@WebServlet("/scoremanager/main/subject_list.action")
+public class SubjectListAction extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // SubjectDaoのインスタンス化
-        SubjectDao subjectDao = new SubjectDao();
+        try {
+            SubjectDao subjectDao = new SubjectDao();
+            List<Subject> subjectList = subjectDao.findAll();
 
-        // 全科目の取得
-        List<Subject> subjectList = subjectDao.findAll();
+            request.setAttribute("subjectList", subjectList);
+            request.getRequestDispatcher("/scoremanager/main/subject_list.jsp").forward(request, response);
 
-        // リクエストスコープにsubjectListを保存
-        request.setAttribute("subjectList", subjectList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
+    }
 
-        // JSPページにフォワード
-        request.getRequestDispatcher("../view/subject_list.jsp").forward(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response); // POSTでもGETと同様の処理をする
     }
 }
-
-
-//package scoremanager.main;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import bean.Subject;
-//import dao.SubjectDao;
-//
-//@WebServlet("/scoremanager/main/subject_list.action")
-//public class SubjectListAction extends HttpServlet {
-//
-//  @Override
-//  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//          throws ServletException, IOException {
-//      try {
-//          // 科目DAOを使って全科目取得
-//          SubjectDao subjectDao = new SubjectDao();
-//          List<Subject> subjectList = subjectDao.findAll();
-//
-//          // リクエスト属性に設定してJSPへ転送
-//          request.setAttribute("subjectList", subjectList);
-//          request.getRequestDispatcher("/WEB-INF/view/subject/list.jsp")
-//                 .forward(request, response);
-//
-//      } catch (Exception e) {
-//          e.printStackTrace();
-//          throw new ServletException("科目一覧の取得に失敗しました。", e);
-//      }
-//  }
-//}
